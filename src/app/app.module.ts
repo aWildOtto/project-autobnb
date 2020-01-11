23e734292477cb3f320d12cc9485a49ce63cd780
ohu <otto_hu@sfu.ca>
2020-01-11 11:42:16 -0800

    add md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { BoardpageComponent } from './boardpage/boardpage.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { OptionCardComponent } from './option-card/option-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { OptionCardComponent } from './option-card/option-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
