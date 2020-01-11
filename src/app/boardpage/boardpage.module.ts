9fed698f87776bb70909d7f0c0707061be0b15d9
Andy Tsang <atsang36@hotmail.com>
2020-01-11 13:17:29 -0800

    added some more proect organization

--- src/app/boardpage/boardpage.module.ts
+++ src/app/boardpage/boardpage.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { BoardpageRoutingModule } from './boardpage-routing.module';
 import { MaterialModule } from '../material.module';
 import { BoardpageComponent } from './boardpage.component';
+import { OptionCardComponent } from '../option-card/option-card.component';
 
 
 @NgModule({
   declarations: [
-    BoardpageComponent
+    BoardpageComponent,
+    OptionCardComponent
   ],
   imports: [
     CommonModule,
