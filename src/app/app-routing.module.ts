ac32c73e22f325392b34626169b53f140291b371
ohu <otto_hu@sfu.ca>
2020-01-11 12:02:09 -0800

    start UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BoardpageComponent } from './boardpage/boardpage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: ':id', component: BoardpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
